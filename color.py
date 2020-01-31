2e552fdc1a46c7e74727d6c0c22819010492526b
Mikolaj Andrzejewski <mikandrzej12321@gmail.com>
2020-01-31 23:20:51 +0100

    added shelfs to bookstands

--- color.py
+++ color.py
@@ -4,3 +4,7 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+
+
+COL_BOOKSTAND = BLACK
+COL_SHELF = WHITE
--- views/bookstand.py
+++ views/bookstand.py
@@ -1,5 +1,6 @@
 import pygame
 
+import color
 from properties import Properties
 
 class Bookstand:
@@ -7,28 +8,41 @@ class Bookstand:
     POS_Y = 0.2
     WIDTH = 0.6
     HEIGHT = 0.6
-
-    COLOR = (0,0,0)
+    BOARD_WIDTH = 0.01
+    SHELFS = 3
+    OBJECTS_ON_SHELF = 5
 
     def __init__(self, properties: Properties):
         self.properties = properties
+
+        objects = []
+        for x in range(self.OBJECTS_ON_SHELF):
+            pass
+        self._objects = objects
+
 
     def draw(self, surface: pygame.Surface):
         self._drawShelf(surface)
 
     def _drawShelf(self, surface: pygame.Surface):
         bookstand = pygame.Rect(self.POS_X * self.properties.WIDTH,
-                            self.POS_X * self.properties.HEIGHT,
+                            self.POS_Y * self.properties.HEIGHT,
                             self.WIDTH * self.properties.WIDTH,
                             self.HEIGHT * self.properties.HEIGHT
                             )
-        pygame.draw.rect(surface, self.COLOR, bookstand)
-        bookstand = pygame.Rect(self.POS_X * self.properties.WIDTH,
-                            self.POS_X * self.properties.HEIGHT,
-                            self.WIDTH * self.properties.WIDTH,
-                            self.HEIGHT * self.properties.HEIGHT
-                            )
-        pygame.draw.rect(surface, self.COLOR, bookstand)
+
+        shelfs = []
+        for x in range(self.SHELFS):
+            shelf = pygame.Rect((self.POS_X + self.BOARD_WIDTH) * self.properties.WIDTH,
+                                ((self.POS_Y + (x * (self.HEIGHT/3))) + self.BOARD_WIDTH) * self.properties.HEIGHT,
+                                (self.WIDTH - (2*self.BOARD_WIDTH)) * self.properties.WIDTH,
+                                ((self.HEIGHT / self.SHELFS) - (2*self.BOARD_WIDTH)) * self.properties.HEIGHT
+                                )
+            shelfs.append(shelf)
+
+        pygame.draw.rect(surface, color.COL_BOOKSTAND, bookstand)
+        for shelf in shelfs:
+            pygame.draw.rect(surface, color.COL_SHELF, shelf)
 
 
 
